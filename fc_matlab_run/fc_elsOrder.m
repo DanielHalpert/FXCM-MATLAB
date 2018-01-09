35e700e81a72f011be055623769910597e1ad87d
Daniel Halpert <dan4783@yahoo.com>
2018-01-09 19:21:48 +0200

    Add files via upload

--- fc_matlab_run/fc_elsOrder.m
+++ fc_matlab_run/fc_elsOrder.m
@@ -3,19 +3,19 @@ fc_msg = 0;
 if not(libisloaded('fc_matlab'))
     display('need login first');
 else
-    [fc_msg] = calllib('fc_matlab','createELSorder', '01549059', 6000, 'B', 'EUR/USD', 'Market', 'GTC', 0, 1.34, 1.1211, 10);
+    [fc_msg] = calllib('fc_matlab','createELSorder', '01549059', 6000, 'B', 'EUR/USD', 'Limit', 'GTD', 1.191, 1.34, 1.1211, 10, '20180112-15:20:00');
 end;    
 
 %{
 Parameters for createEntryOrder function:
-accountNumber, trade size, side, symbol, order type, TIF, price, limiPrice, stopPrice, trailing stop 
+accountNumber, trade size, side, symbol, order type, TIF, price, limiPrice, stopPrice, trailing stop, expDate 
 
 accountNumber - account can be found in Trading Station
 trade size - for FX min. 1000 or account min. trade size
 side - B=Buy, S=Sell
 symbol - any symbol available in your account
 order type - can be one of: Market, Stop, Limit
-TIF (Time in Force) parameters: GTC, IOC, FOK 
+TIF (Time in Force) parameters: GTC, GTD, IOC, FOK 
 price - the open position price, if market order the price will be ignored
 limitPrice - it's the profit, if main position is Buy then limitPrice should be above the market 
 stopPrice - Stop Loss, should be below the market in case of long position
@@ -24,4 +24,6 @@ trailing stop - attached to stopPrice, the number is in pips, example for long p
     Limit = 130
     Stop = 70
     when Bid = 110, the Stop will move to 80
+expDate - expiration date and time in format - yyyyMMdd-HH:mm:ss", only for
+            orders where TIF = GTD 
 %}
--- fc_matlab_run/fc_matlab.h
+++ fc_matlab_run/fc_matlab.h
@@ -14,18 +14,17 @@ __declspec(dllexport)  void Register_Callback(fnCallBackFunc func);
 
 
 DECLDIR int  __stdcall login_fc(const char* username, const char* pass, const char* connection, const char* url);
-DECLDIR bool  __stdcall getHistoryPrices(const char *sInstrument, const char *sTimeframe,  const char* dtFrom,  const char* dtTo, const char* fileName);
+DECLDIR bool __stdcall getHistoryPrices(const char *sInstrument, const char *sTimeframe, const char* dtFrom, const char* dtTo, const char* fileName);
 DECLDIR int  __stdcall logout_fc();
 DECLDIR int  __stdcall createMarketOrder(const char *sAccountID, int iAmount, const char *sBuySell, const char *tif, const char *instrument, const char *ticketNum);
-DECLDIR int __stdcall createEntryOrder(const char *sAccountID, int iAmount, const char *sBuySell, const char *tif, const char *instrument, const char* StopLimit, const double price, const char *ticketNum);
-DECLDIR int __stdcall createELSorder(const char *sAccount, int iAmount, const char *sBuySell, const char *instrument, const char *ordType1, const char *tif, const double price, const double limitPrice, const double stopPrice, const int trailStop);
-DECLDIR int __stdcall getRealTimePrices(const char *instrument);
-DECLDIR int __stdcall stopPrices(const char *instrument);
-DECLDIR int __stdcall printOpenPositions(const char *sAccount, const char *fileName);
-DECLDIR int __stdcall printOrders(const char *sAccount, const char *fileName);
+DECLDIR int  __stdcall createEntryOrder(const char *sAccountID, int iAmount, const char *sBuySell, const char *tif, const char *instrument, const char* StopLimit, const double price, const char* ticketNum, const char* expDate);
+DECLDIR int  __stdcall createELSorder(const char *sAccount, int iAmount, const char *sBuySell, const char *instrument, const char *ordType1, const char *tif, const double price, const double limitPrice, const double stopPrice, const int trailStop, const char* expDate);
+DECLDIR int  __stdcall getRealTimePrices(const char *instrument);
+DECLDIR int  __stdcall stopPrices(const char *instrument);
+DECLDIR int  __stdcall printOpenPositions(const char *sAccount, const char *fileName);
+DECLDIR int  __stdcall printOrders(const char *sAccount, const char *fileName);
 DECLDIR int  __stdcall changeELSstopLimit(const char *sAccount, const char *tradeId, const double limitPrice, const double stopPrice);
 DECLDIR int  __stdcall changeOrder(const char *sAccount, const char *orderId, const double price, const int amount);
-
 
 void __stdcall fc_mexCallback(char *msg, double msgType);
 //void printLog(std::string str);
@@ -33,4 +32,3 @@ void __stdcall fc_mexCallback(char *msg, double msgType);
 #ifdef __cplusplus
 }
 #endif
-
